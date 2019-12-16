ca6d66e8d586091084ac6f85765a60dc2100e904
egee-irl <brian@egee.io>
2019-12-16 03:07:07 -0800

    updated defaults

--- main.rb
+++ main.rb
@@ -10,17 +10,17 @@ EM.run do
   ws.on :open do |event|
     p [:open]
     puts event
-    # ws.send "{Identifier: 1, Message: 'inventory.giveall pumpkin 5'}"
   end
 
   # inventory.giveto eg ammo.shotgun.fire 1
 
   ws.on :message do |event|
     message = JSON.parse(event.data)
-    if message['Message'].include? 'joined ['
-      ws.send "{Identifier: 1, Message: 'inventory.giveall wood 1000'}"
-      ws.send "{Identifier: 1, Message: 'inventory.giveall lowgradefuel 100'}"
-      ws.send "{Identifier: 1, Message: 'inventory.giveall pistol.semiauto 1'}"
+    if message['Message'].include? 'joined from ip'
+      ws.send "{Identifier: 1, Message: 'inventory.giveall pumpkin 5'}"
+      ws.send "{Identifier: 1, Message: 'inventory.giveall wood 10000'}"
+      ws.send "{Identifier: 1, Message: 'inventory.giveall lowgradefuel 250'}"
+      ws.send "{Identifier: 1, Message: 'inventory.giveall sleepingbag 1'}"
       ws.send "{Identifier: 1, Message: 'say If you need more items, join our Discord server & ask in #rust. Invite link: https://discord.gg/EMbcgR8'}"
     elsif message['Message'] == '!help'
       ws.send "{Identifier: 1, Message: 'say Commands will be added next wipe. In the mean time, join our Discord server to request items.'}"
